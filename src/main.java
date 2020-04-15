3c651de666b70b4641a6d316d263203906f76204
Trevor Cunnigham <trejcunn@iu.edu>
2020-04-15 11:25:57 -0400

    added random to test

--- src/main.java
+++ src/main.java
@@ -1,5 +1,8 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("AAA");
+        int i = 0;
+        i++;i++;i++;
+        System.out.println(i);
     }
 }
