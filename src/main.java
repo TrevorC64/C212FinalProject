f2af953497f7d5979b2b3b3e92cffab5630fb865
TrevorC64 <55000457+TrevorC64@users.noreply.github.com>
2020-04-15 11:27:01 -0400

    Update main.java
    saa

--- src/main.java
+++ src/main.java
@@ -2,7 +2,7 @@ public class main {
     public static void main(String[] args) {
         System.out.println("AAA");
         int i = 0;
-        i++;i++;i++;
+        i++;i++;
         System.out.println(i);
     }
 }
