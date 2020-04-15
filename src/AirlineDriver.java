badd638bbaa9843d9a89068a734df4f654dc6c1c
Trevor Cunnigham <trejcunn@iu.edu>
2020-04-15 12:14:46 -0400

    Airline Driver and User class updated

--- src/AirlineDriver.java
+++ src/AirlineDriver.java
@@ -1,2 +1,15 @@
 public class AirlineDriver {
+
+    public static void main(String[] args) {
+        //TODO Implement user inputs and system navigation
+        //this is where the program will be ran
+
+        //1. initialize system
+
+        //2. prompt for a login
+
+        //If Customer:
+
+        //If Airline:
+    }
 }
